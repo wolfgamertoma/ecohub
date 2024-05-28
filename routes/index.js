65d944f7f97b5ed2e91fbe2a19260b199b71e39b
Radu <radulupu.ase@gmail.com>
2024-05-28 23:16:39 +0300

    Add about functionality

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
   res.render('index', { body: '<h2>Bine ai venit la platforma noastră!</h2>' });
 });
 
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
 module.exports = router;
